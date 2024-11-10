0a3c5e7d8b185c65bf20f775806d7f6cb4f9b757
NamanGupta123 <namangupta0330@gmail.com>
2024-11-10 19:58:08 +0530

    Updated server

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ def home():
 
 @app.after_request
 def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin', 'chrome-extension://*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
