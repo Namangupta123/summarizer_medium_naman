f1945d9d32e9341613549be4045f9b0d22975de6
NamanGupta123 <namangupta0330@gmail.com>
2024-11-16 13:41:57 +0530

    Hope4

--- server.py
+++ server.py
@@ -29,25 +29,21 @@ CORS(app,
          r"/*": {
              "origins": ["https://medium.com", "https://*.medium.com", "chrome-extension://*"],
              "methods": ["GET", "POST", "OPTIONS"],
-             "allow_headers": ["Content-Type", "Authorization", "Accept", "Origin"],
-             "supports_credentials": True,
-             "expose_headers": ["Content-Type", "Authorization"],
-             "max_age": 3600,
-             "vary_header": True
+             "allow_headers": ["Content-Type", "Authorization", "Accept"],
+             "max_age": 3600
          }
      })
 
-
 @app.after_request
 def after_request(response):
-    origin = request.headers.get('Origin', '')
-    if origin.startswith('https://medium.com'):
-        response.headers.add('Access-Control-Allow-Origin', origin)
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
-    response.headers.add('Access-Control-Allow-Credentials', 'true')
-    response.headers.add('Access-Control-Max-Age', '3600')
+    origin = request.headers.get('Origin')
+    if origin:
+        response.headers['Access-Control-Allow-Origin'] = origin
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, Accept'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Max-Age'] = '3600'
     return response
+
 
 # Configure timezone
 IST = pytz.timezone('Asia/Kolkata')
